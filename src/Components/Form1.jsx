f4f07a8b645aade09ea1001058032bae357f5b56
Ankit Mishra <92905082+ankitmishra2612@users.noreply.github.com>
2024-06-14 13:30:12 +0530

    Update Form1.jsx

--- src/Components/Form1.jsx
+++ src/Components/Form1.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
+import PropTypes from "prop-types" ;
 
 const Form1 = ({ formData, setFormData, nextStep }) => {
   const [errors, setErrors] = useState({});
